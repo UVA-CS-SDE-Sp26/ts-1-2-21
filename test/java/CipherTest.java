2d4d8d2a63d7cc2cf6b248e99abb5fc538eef57a
anaSuzic <suzicka01@gmail.com>
2026-02-13 09:52:38 -0500

    tests

--- test/java/CipherTest.java
+++ test/java/CipherTest.java
@@ -1,56 +1,35 @@
-package java;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.crypto.Cipher;
-import java.io.File;
-import java.io.FileWriter;
-
+import package_name.Cipher;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CipherTest {
+
     private Cipher cipher;
 
     @BeforeEach
     void setUp() throws Exception {
-        File file = File.createTempFile("key", ".txt");
-        FileWriter writer = new FileWriter(file);
-        writer.write("abc\n");
-        writer.write("xyz\n");
-        writer.close();
-
-        var keyPath = file.getAbsolutePath();
-        cipher = new Cipher(keyPath);
+        cipher = new Cipher("ciphers/key.txt");
     }
 
     @Test
-    void testCipherLoadsKey() {
-        assertNotNull(cipher, "Cipher should be created with a valid key file");
+    void testCipherLoads() {
+        assertNotNull(cipher);
     }
 
     @Test
-    void testDecipherSimpleWord() {
-        String result = cipher.decipher("xyz");
-        assertEquals("abc", result, "xyz should map back to abc");
+    void testDecipherSingleCharacter() {
+        assertEquals("a", cipher.decipher("b"));
+    }
+
+    @Test
+    void testDecipherWord() {
+        assertEquals("0ab", cipher.decipher("abc"));
     }
 
     @Test
     void testDecipherKeepsUnknownCharacters() {
-        String result = cipher.decipher("x!y?z");
-        assertEquals("a!b?c", result, "Non-key characters should remain unchanged");
-    }
-
-    @Test
-    void testInvalidKeyLengthThrowsError() throws Exception {
-        File file = File.createTempFile("badkey", ".txt");
-        FileWriter writer = new FileWriter(file);
-        writer.write("abcd\n");
-        writer.write("xyz\n");
-        writer.close();
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Cipher(file.getAbsolutePath());
-        }, "Should throw error when key lines are different lengths");
+        assertEquals("a!", cipher.decipher("b!"));
     }
 }
