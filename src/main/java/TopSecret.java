65d5ca09841e692c5fa33263fafbbb2f770976f7
wjcs1 <wjcs1zhan@gmail.com>
2026-02-13 10:24:22 -0500

    removed main method overhead

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -4,22 +4,7 @@
 import java.util.*;
 public class TopSecret {
     public static void main(String[] args) {
-
         UserInterface user = new UserInterface();
-        int[] commandLineInfo = user.commandLineInterface(args);
-
-        if (commandLineInfo[0] == 0) {
-            List<String> files = FileHandler.listFiles();
-            for (int i = 0; i < files.size(); i++) {
-                System.out.println(String.format("%02d %s", i + 1, files.get(i)));
-            }
-        }
-        else if (commandLineInfo[0] == 1 || commandLineInfo[0] == 2) {
-            String content = FileHandler.readFile(commandLineInfo[1]);
-            System.out.println(content);
-        }
-        else {
-            System.out.println("Invalid command line arguments.");
-        }
+        user.commandLineInterface(args);
     }
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,28 +1,29 @@
 public class UserInterface {
     // takes in command line args, returns number of command line args (index 0), file to read (if applicable, index 0), and cipher to use (if applicable, index 2)
-    public int[] commandLineInterface(String[] info) {
-        int[] commandLineArgs = {0, 0, 0};
+    public void commandLineInterface(String[] info) {
+        // for program controller
+        int fileToBeRead;
+        String keyFile;
+
+        ProgramController pc = new ProgramController(new FileHandler(), new Cipher());
 
         switch(info.length) {
             case 0:
                 // no command line args
-                break;
+                pc.listFiles();
             case 1:
                 // one command line arg, file number
-                commandLineArgs[0] = 1;
-                commandLineArgs[1] = Integer.parseInt(info[0]);
+                fileToBeRead = Integer.parseInt(info[0]);
+                pc.getFileContent(fileToBeRead);
                 break;
             case 2:
                 // two command line args, file number and cipher to use for decryption
-                commandLineArgs[0] = 2;
-                commandLineArgs[1] = Integer.parseInt(info[0]);
-                commandLineArgs[2] = Integer.parseInt(info[1]);
+                fileToBeRead = Integer.parseInt(info[0]);
+                keyFile = info[1];
                 break;
             default:
                 // user entered some invalid number of command line args
                 System.out.println("Please enter 0-2 command line arguments.");
         }
-
-        return commandLineArgs;
     }
 }
