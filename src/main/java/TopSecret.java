ffd7caf917abc556dcb9a97652bdd3322c0569f2
Tessa <tessajosephh@gmail.com>
2026-02-13 10:21:17 -0500

    non static file handler outside of base fldr

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -8,18 +8,5 @@ public class TopSecret {
         UserInterface user = new UserInterface();
         int[] commandLineInfo = user.commandLineInterface(args);
 
-        if (commandLineInfo[0] == 0) {
-            List<String> files = FileHandler.listFiles();
-            for (int i = 0; i < files.size(); i++) {
-                System.out.println(String.format("%02d %s", i + 1, files.get(i)));
-            }
-        }
-        else if (commandLineInfo[0] == 1 || commandLineInfo[0] == 2) {
-            String content = FileHandler.readFile(commandLineInfo[1]);
-            System.out.println(content);
-        }
-        else {
-            System.out.println("Invalid command line arguments.");
-        }
     }
 }
